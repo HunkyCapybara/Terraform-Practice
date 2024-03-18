863477bdfb5fb7626dee41d9dba79302f50eb403
HunkyCapybara <nagaraju.gajjela18@gmail.com>
2024-03-18 12:53:51 +0530

    added the public ip command

--- day-6-datasource-amifilter/main.tf
+++ day-6-datasource-amifilter/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "smtng" {
     instance_type = "t2.micro"
     security_groups = [data.aws_security_group.smtng.id]
     subnet_id = data.aws_subnet.smtng.id
+    associate_public_ip_address = true
     tags = {
      Name = "Smtng" 
     }
