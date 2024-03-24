7019af64f6ab8fd99d1a674f2b383ef3a472bfaf
HunkyCapybara <nagaraju.gajjela18@gmail.com>
2024-03-24 18:25:44 +0530

    workspace updates

---
+++ day-8-workspace/main.tf
@@ -0,0 +1,7 @@
+resource "aws_s3_bucket" "worksapce" {
+    bucket = "sdfghjklasdfghjklsdfghjkasdfghj"
+    
+  
+}
+
+#So basically workspace can create multiple state files in the same directory and that can be done by switching into the respective workspaces by the workspace select command
